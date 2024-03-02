253e2f5759d85465ed6bd0fdcdfce5613288b3e5
bobbyhadz <opmftw@gmail.com>
2024-03-02 10:01:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// ReferenceError: moment is not defined in JavaScript
+
+// Make sure moment is installed with `npm install moment`
+
+import moment from 'moment';
+
+console.log(moment().format());
